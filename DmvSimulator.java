3034eaaea61ea78158e504ae1d8c47db93a5f889
aamnahmalik <aamnah@icloud.com>
2023-03-05 17:50:25 -0500

    increased wait time from 100 to 200 calls

--- DmvSimulator.java
+++ DmvSimulator.java
@@ -4,10 +4,10 @@ public class DmvSimulator
 {
     public static void main(String[] args)
     {
-        int userCallNumber = (int)(100 * Math.random() + 1);
+        int userCallNumber = (int)(200 * Math.random() + 1);
         System.out.println("Hello! Welcome to the DMV. \nYour number is " + userCallNumber + ". Please wait until this number is called.");
 
-        for(int currentCallNumber = (userCallNumber + 1); currentCallNumber <= 100; currentCallNumber++)
+        for(int currentCallNumber = (userCallNumber + 1); currentCallNumber <= 200; currentCallNumber++)
         {
             System.out.println("Current call: " + currentCallNumber);
         }
