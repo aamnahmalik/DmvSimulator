11c2070155227de8df440b59da74b0d20c029405
aamnahmalik <aamnah@icloud.com>
2023-03-05 17:40:24 -0500

    set up DMV welcome, calls, paperwork message

--- DmvSimulator.java
+++ DmvSimulator.java
@@ -4,6 +4,18 @@ public class DmvSimulator
 {
     public static void main(String[] args)
     {
-        
+        int userCallNumber = (int)(100 * Math.random() + 1);
+        System.out.println("Hello! Welcome to the DMV. \nYour number is " + userCallNumber + ". Please wait until this number is called.");
+
+        for(int currentCallNumber = (userCallNumber + 1); currentCallNumber <= 100; currentCallNumber++)
+        {
+            System.out.println("Current call: " + currentCallNumber);
+        }
+        for(int currentCallNumber = 1; currentCallNumber <= userCallNumber; currentCallNumber++)
+        {
+            System.out.println("Current call: " + currentCallNumber);
+        }
+
+        System.out.println("I'm sorry, you do not have the required paperwork.");
     }
 }
