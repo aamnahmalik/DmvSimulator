36bf073446c47c39bfe3aa017d13df3ffbdc8f29
AlecGoriup <113146110+AlecGoriup@users.noreply.github.com>
2023-03-07 15:03:41 -0500

    Default Formatting, without auto-formatting

--- DmvSimulator.java
+++ DmvSimulator.java
@@ -1,21 +1,22 @@
 package DmvSimulator;
 
-public class DmvSimulator 
+public class DmvSimulator
 {
-    public static void main(String[] args) 
+    public static void main(String[] args)
     {
-        int userCallNumber = (int)(200 * Math.random() + 1);
+        int userCallNumber = (int)(100 * Math.random() + 1);
         System.out.println("Hello! Welcome to the DMV. \nYour number is " + userCallNumber + ". Please wait until this number is called.");
 
-        for(int currentCallNumber = (userCallNumber + 1); currentCallNumber <= 200; currentCallNumber++) 
+        for(int currentCallNumber = (userCallNumber + 1); currentCallNumber <= 100; currentCallNumber++)
         {
             System.out.println("Current call: " + currentCallNumber);
         }
-        for (int currentCallNumber = 1; currentCallNumber <= userCallNumber; currentCallNumber++) 
+        for(int currentCallNumber = 1; currentCallNumber <= userCallNumber; currentCallNumber++)
         {
             System.out.println("Current call: " + currentCallNumber);
         }
 
-        System.out.println("You do not have the required paperwork. We can't do anything without your paperwork!");
+        
+        System.out.println("I'm sorry, you do not have the required paperwork.");
     }
 }
