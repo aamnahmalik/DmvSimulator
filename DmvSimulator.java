56d2b499a93fa812de35eccc3c0a0e379281b0c7
aamnahmalik <aamnah@icloud.com>
2023-03-07 14:29:43 -0500

    changed formatting

--- DmvSimulator.java
+++ DmvSimulator.java
@@ -1,15 +1,18 @@
 package DmvSimulator;
 
-public class DmvSimulator {
-    public static void main(String[] args) {
+public class DmvSimulator 
+{
+    public static void main(String[] args) 
+    {
         int userCallNumber = (int)(200 * Math.random() + 1);
-        System.out.println("Hello! Welcome to the DMV. \nYour number is " + userCallNumber 
-                + ". Please wait until this number is called.");
+        System.out.println("Hello! Welcome to the DMV. \nYour number is " + userCallNumber + ". Please wait until this number is called.");
 
-        for(int currentCallNumber = (userCallNumber + 1); currentCallNumber <= 200; currentCallNumber++) {
+        for(int currentCallNumber = (userCallNumber + 1); currentCallNumber <= 200; currentCallNumber++) 
+        {
             System.out.println("Current call: " + currentCallNumber);
         }
-        for (int currentCallNumber = 1; currentCallNumber <= userCallNumber; currentCallNumber++) {
+        for (int currentCallNumber = 1; currentCallNumber <= userCallNumber; currentCallNumber++) 
+        {
             System.out.println("Current call: " + currentCallNumber);
         }
 
